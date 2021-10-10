07feb8f7be56a6d71ccdb5e4f752161909f19ca0
Lucas Koch <luko907@hotmail.com>
2021-10-10 12:12:19 -0300

    update card btn, title and weather img

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -5,12 +5,18 @@ export default function Card() {
   return (
     <div className={Styles.cardContainer}>
       <div className={Styles.card}>
-        <div>
-          <button>x</button>
+        <div className={Styles.divBtn}>
+          <button className={Styles.btnContent}>x</button>
         </div>
-        <span>Londres</span>
-        <div>
-          <img src="" width="80" height="80" alt="" />
+        <span className={Styles.citie}>London</span>
+        <div className={Styles.actual_Weather}>
+          <img
+            className={Styles.weatherImg}
+            src=""
+            width="80"
+            height="80"
+            alt=""
+          />
           <div>
             <span>32°</span>
             <span>Clouds</span>
@@ -29,4 +35,33 @@ export default function Card() {
       </div>
     </div>
   );
+}
+
+{
+  /* <div className={Styles.cardContainer}>
+<div className={Styles.card}>
+  <div>
+    <button>x</button>
+  </div>
+  <span>Londres</span>
+  <div>
+    <img src="" width="80" height="80" alt="" />
+    <div>
+      <span>32°</span>
+      <span>Clouds</span>
+    </div>
+  </div>
+  <div>
+    <div>
+      <span>Min</span>
+      <span>20°</span>
+    </div>
+    <div>
+      <span>Max</span>
+      <span>34°</span>
+    </div>
+  </div>
+</div>
+</div>
+); */
 }
