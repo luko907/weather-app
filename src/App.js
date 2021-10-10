4c9b3b2ff276857563c80a9366874e7eee2123b1
Lucas Koch <luko907@hotmail.com>
2021-10-10 11:09:02 -0300

    create card component and import to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Card from "./Card/Card.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Card/Card.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div>
+      <div>
+        <div>
+          <button>x</button>
+        </div>
+        <span></span>
+        <div>
+          <img src="" width="80" height="80" alt="" />
+          <div>
+            <span>°</span>
+            <span></span>
+          </div>
+        </div>
+        <div>
+          <div>
+            <span>Min</span>
+            <span>°</span>
+          </div>
+          <div>
+            <span>Max</span>
+            <span>°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
