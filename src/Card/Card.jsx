e2e3edfc757b35acf8c58897ed8074d47fa753a3
Lucas Koch <luko907@hotmail.com>
2021-10-11 14:18:02 -0300

    fix card data

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import Styles from "./Card.module.css";
-
-export default function Card(props) {
+export default function Card({
+  min,
+  max,
+  name,
+  img,
+  onClose,
+  id,
+  temp,
+  weather,
+}) {
   return (
     <div className={Styles.cardContainer}>
       <div className={Styles.card}>
         <div className={Styles.divBtn}>
-          <button className={Styles.btnContent}>x</button>
+          <button className={Styles.btnContent} onClick={onClose}>
+            x
+          </button>
         </div>
-        <span className={Styles.citie}>{props.name}</span>
+        <span className={Styles.citie}>{name}</span>
         <div className={Styles.actual_Weather}>
           <img
             className={Styles.weatherImg}
-            src={props.img}
+            src={"http://openweathermap.org/img/wn/" + img + "@2x.png"}
             width="80"
             height="80"
-            alt={props.weather}
+            alt={`${img}`}
           />
           <div className={Styles.actualTempWeather}>
-            <span>{props.temp}°</span>
-            <span className={Styles.weatherInfo}>{props.weather}</span>
+            <span>{temp.toFixed(1)}°</span>
+            <span className={Styles.weatherInfo}>{weather}</span>
           </div>
         </div>
         <div className={Styles.maxMin}>
           <div className={Styles.min}>
             <span>Min</span>
-            <span>{props.min}°</span>
+            <span>{min.toFixed(1)}°</span>
           </div>
           <div className={Styles.max}>
             <span>Max</span>
-            <span>{props.max}°</span>
+            <span>{max.toFixed(1)}°</span>
           </div>
         </div>
       </div>
