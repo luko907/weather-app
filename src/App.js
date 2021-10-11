594eb9cc79962f079e6ee076019486865d8eab82
Lucas Koch <luko907@hotmail.com>
2021-10-11 12:52:57 -0300

    connect API and get data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Cards from "./Cards/Cards.jsx";
 import SearchBar from "./SearchBar/Searchbar.jsx";
 import "./App.css";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  function onSearch(citie) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${citie}&appid=${process.env.REACT_APP_API}&units=metric`
+    )
+      .then((r) => r.json())
+      .then((response) => {
+        const res = {
+          min: response.main.temp_min,
+          max: response.main.temp_max,
+          img: response.weather[0].icon,
+          wind: response.wind.speed,
+          temp: response.main.temp,
+          name: response.name,
+          weather: response.weather[0].main,
+          clouds: response.clouds.all,
+          latitud: response.coord.lat,
+          timezone: response.timezone,
+          longitud: response.coord.lon,
+          id: response.id,
+        };
+        setCities((oldCities) => [...oldCities, res]);
+      });
+  }
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar onSearch={onSearch} />
       <div className="cardsContainer">
-        <Cards />
+        <Cards cities={cities} />
       </div>
     </div>
   );
--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -1,27 +1,7 @@
 import React from "react";
 import Card from "../Card/Card";
 
-export default function Cards() {
-  const cities = [
-    {
-      name: "London",
-      temp: 28,
-      min: 20,
-      max: 35,
-      weather: "Clouds",
-      img: "https://cdn-icons-png.flaticon.com/512/990/990462.png",
-      id: 1,
-    },
-    {
-      name: "New York",
-      temp: 22,
-      min: 15,
-      max: 27,
-      weather: "Sunny",
-      img: "https://cdn-icons-png.flaticon.com/512/2698/2698194.png",
-      id: 2,
-    },
-  ];
+export default function Cards({ cities }) {
   return (
     <div>
       {cities.map((cities, i) => (
