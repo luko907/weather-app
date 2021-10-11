60e76dab03a2ae33618831f3d94bfa507c9eb93a
Lucas Koch <luko907@hotmail.com>
2021-10-11 10:41:14 -0300

    add searchbar styles

--- src/SearchBar/Searchbar.jsx
+++ src/SearchBar/Searchbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import SearchStyle from "./Searchbar.module.css";
 
 export default function SearchBar() {
   return (
     <div>
       <form>
-        <div>
-          <div>
-            <input type="text" placeholder="Agregá país o ciudad . . ." />
-            <button type="submit">+</button>
+        <div className={SearchStyle.containerSearch}>
+          <div className={SearchStyle.inputAndBtn}>
+            <input
+              type="text"
+              className={SearchStyle.inputSearch}
+              placeholder="Agregá país o ciudad . . ."
+            />
+            <button className={SearchStyle.searchBarBtn} type="submit">
+              +
+            </button>
           </div>
         </div>
       </form>
