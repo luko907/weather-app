053de191ce5a76fb2d16e4dba8e619a054d320ec
Lucas Koch <luko907@hotmail.com>
2021-10-11 15:39:09 -0300

    fix app response when not found and blocking repetead cities

--- src/App.js
+++ src/App.js
@@ -5,27 +5,43 @@ import "./App.css";
 
 function App() {
   const [cities, setCities] = useState([]);
-  function onSearch(citie) {
+  function onSearch(ciudad) {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${citie}&appid=${process.env.REACT_APP_API}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.REACT_APP_API}&units=metric`
     )
       .then((r) => r.json())
       .then((response) => {
-        const res = {
-          min: response.main.temp_min,
-          max: response.main.temp_max,
-          img: response.weather[0].icon,
-          wind: response.wind.speed,
-          temp: response.main.temp,
-          name: response.name,
-          weather: response.weather[0].main,
-          clouds: response.clouds.all,
-          latitud: response.coord.lat,
-          timezone: response.timezone,
-          longitud: response.coord.lon,
-          id: response.id,
-        };
-        setCities((oldCities) => [...oldCities, res]);
+        if (response.main !== undefined) {
+          const res = {
+            min: response.main.temp_min,
+            max: response.main.temp_max,
+            img: response.weather[0].icon,
+            wind: response.wind.speed,
+            temp: response.main.temp,
+            name: response.name,
+            weather: response.weather[0].main,
+            clouds: response.clouds.all,
+            latitud: response.coord.lat,
+            timezone: response.timezone,
+            longitud: response.coord.lon,
+            id: response.id,
+          };
+
+          let found = false;
+          for (let i = 0; i < cities.length; i++) {
+            if (cities[i].name === res.name) {
+              found = true;
+              break;
+            }
+          }
+          if (found) {
+            alert("Selecciona otra ciudad");
+          } else {
+            setCities((oldCities) => [...oldCities, res]);
+          }
+        } else {
+          alert("Ciudad no encontrada");
+        }
       });
   }
   return (
