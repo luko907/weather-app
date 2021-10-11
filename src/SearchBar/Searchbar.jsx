6058dbbbc3db413fb894fda6d083ad55570ec427
Lucas Koch <luko907@hotmail.com>
2021-10-11 13:27:39 -0300

    add searchbar functionality

--- src/SearchBar/Searchbar.jsx
+++ src/SearchBar/Searchbar.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchStyle from "./Searchbar.module.css";
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSearch(name);
+    setName("");
+  }
+
   return (
     <div>
-      <form>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <div className={SearchStyle.containerSearch}>
           <div className={SearchStyle.inputAndBtn}>
             <input
               type="text"
+              value={name}
               className={SearchStyle.inputSearch}
+              onChange={(e) => handleChange(e)}
               placeholder="Agregá país o ciudad . . ."
             />
             <button className={SearchStyle.searchBarBtn} type="submit">
