b68ce6d305db179c510e50662676c1381495c914
Lucas Koch <luko907@hotmail.com>
2021-10-12 12:25:30 -0300

    initialize stars styles

--- src/Stars/Stars.jsx
+++ src/Stars/Stars.jsx
@@ -1,16 +1,59 @@
 import React from "react";
-
+import "./Stars.scss";
 export default function Stars() {
-  const getStars = () => {
-    let content = [];
-    for (let i = 0; i < 50; i++) {
-      content.push(<div className="star" key={i}></div>);
-    }
-    return content;
-  };
   return (
     <div>
-      <div>{getStars()}</div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
+      <div className="stars"></div>
     </div>
   );
 }
